ab53178f40a1cb600c8a0fd9ac50014955483391
Dennison Chalacan <134254875+DennisonCc@users.noreply.github.com>
2025-07-03 22:37:25 -0500

    Google Oauth token management

---
+++ middleware/auth.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+
+// Middleware para verificar el token JWT
+const verifyToken = (req, res, next) => {
+    const token = req.header('Authorization')?.replace('Bearer ', '');
+    
+    if (!token) {
+        return res.status(401).json({ message: 'Acceso denegado. No se proporcionó token.' });
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+        next();
+    } catch (error) {
+        res.status(403).json({ message: 'Token inválido.' });
+    }
+};
+
+module.exports = { verifyToken };
